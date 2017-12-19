fb94e5ef5d178f75662d56c06235eb4feafd2b41
Puneet Billing <billingpuneet.13@gmail.com>
2017-12-19 18:42:17 -0500

    getting usermedia twice -- to make predict real time.. need to check this part later

--- partial/MainPage/MainPage.js
+++ partial/MainPage/MainPage.js
@@ -83,7 +83,7 @@ $scope.predictMe = function(){
       console.log('hey, flag has changed!',$scope.flag);
        if(true) //$scope.flag
        {
-        $timeout(countUp, 1000);
+        $timeout(countUp, 2000);
 
       }
 };
@@ -146,7 +146,7 @@ $scope.green = function () {
       photo.setAttribute('src', data);
       dataList.push(data)
     }
-    photo.setAttribute('src', data);
+   // photo.setAttribute('src', data);
     console.log('befr calling sav method')
 
     var dataToSend  = {
@@ -162,16 +162,18 @@ $scope.green = function () {
 
 
         });
-
+    photo.setAttribute('src', data);
 
     } else {
       clearphoto();
     }
   }
- $scope.move = function (barId){
+
+  $scope.movefast = function (barId){
+  console.log('calling moveee')
   var elem = document.getElementById(barId);
   var width = 0;
-  var id = setInterval(frame,1000);
+  var id = setInterval(frame,80);
   function frame() {
 /*    if (width >= 100) {*/
     if (valueSet) {
@@ -190,12 +192,59 @@ $scope.green = function () {
                 valueSet = false;
 
 };
+ $scope.move = function (barId){
+  var elem = document.getElementById(barId);
+  var width = 0;
+  var id = setInterval(frame,2000);
+  function frame() {
+/*    if (width >= 100) {*/
+    if (valueSet) {
+      clearInterval(id);
+      elem.style.width = '100' + '%';
+      elem.innerHTML = '100' + '%';
+    } else {
+      width++;
+      if(width<=100)
+      {
+      elem.style.width = width + '%';
+      elem.innerHTML = width * 1  + '%';
+      }
+    }
+  }
+              console.log('setting flag');
+                $scope.flag = true;
+                console.log($scope.flag);
+                valueSet = false;
+
+};
 
  $scope.timeInMs = 0;
 
     var countUp = function()
     {
-
+          $timeout(countUp,2000);
+          navigator.getMedia(
+      {
+        video: true,
+        audio: false
+      },
+      function(stream) {
+        if (navigator.mozGetUserMedia)
+        {
+          video.mozSrcObject = stream;
+        }
+        else
+        {
+          var vendorURL = window.URL || window.webkitURL;
+          video.src = vendorURL.createObjectURL(stream);
+          //console.log('video.src' ,video.src);
+        }
+        video.play();
+      },
+      function(err) {
+        console.log("An error occured! " + err);
+      }
+    );
         console.log('calling send predictImg')
         var context = canvas.getContext('2d');
         var predictList = [];
@@ -203,34 +252,35 @@ $scope.green = function () {
             canvas.width = width;
             canvas.height = height;
             context.drawImage(video, 0, 0, width, height);
-            var data = canvas.toDataURL('image/png');
+            var mydata = canvas.toDataURL('image/png');
             // photo.setAttribute('src', data);
-            predictList.push(data)
+            predictList.push(mydata)
 
         //console.log('datafrmPred',data)
         var dataToSend  = {
             /*id = "1"*/
-            content : data
+            content : mydata
 
         }
         console.log('calling predict method-----')
-        MainService.predict(dataToSend).then(function (data) {
+        MainService.predict(dataToSend).then(function (sdata) {
+
             console.log('hooooooooolaaaaaaaaaaaaa');
-            console.log(data);
-            console.log(data.message.id)
-            if(data.message.id =="0")
+            console.log(sdata);
+            console.log(sdata.message.id)
+            if(sdata.message.id =="0")
             {
                  $scope.rcategory = "Red"
                  $scope.gcategory = ""
                  $scope.bcategory = ""
             }
-            else if(data.message.id =="1")
+            else if(sdata.message.id =="1")
             {
                  $scope.rcategory = ""
                  $scope.gcategory = "Green"
                  $scope.bcategory = ""
             }
-            else if(data.message.id =="2")
+            else if(sdata.message.id =="2")
             {
                  $scope.rcategory = ""
                  $scope.gcategory = ""
@@ -247,6 +297,6 @@ $scope.green = function () {
        }
 
 
-        $timeout(countUp,2000);
+
     }
 });
